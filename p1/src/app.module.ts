0b34c1f058e9da34aeb8110bb86efd49396cab05
fsquijada <2289458600101@ingenieria.usac.edu.gt>
2026-02-05 21:08:42 -0600

    Recursos de usuarios y de cliente http

--- p1/src/app.module.ts
+++ p1/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { HttpModule } from './http/http.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true })
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    HttpModule
   ],
+  providers: [PrismaService],
 })
 export class AppModule {}
